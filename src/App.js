294fc5da9197005837d804c17af4006d903268fd
klyde07 <106705835+klyde07@users.noreply.github.com>
2025-09-20 23:58:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import axios from 'axios';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const apiUrl = process.env.REACT_APP_API_URL || 'https://ecommerce-backend-production-ce4e.up.railway.app';
 
   useEffect(() => {
-    axios.get('https://ecommerce-backend-production-ce4e.up.railway.app/products')
+    axios.get(`${apiUrl}/products`)
       .then(response => setProducts(response.data))
       .catch(error => console.error('Erreur:', error));
   }, []);
