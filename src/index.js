6a6da1c5e9af991d3c2366bd6bba116b169cac5d
klyde07 <106705835+klyde07@users.noreply.github.com>
2025-09-23 09:35:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css'; // Assure-toi d'avoir un index.css si besoin
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
